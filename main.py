e6dbb6f67286a5383e8cb6d6453d6dddf13f8a83
Chris Parsonson <cjparsonson@gmail.com>
2021-01-11 19:45:35 -0300

    main.py updated

--- main.py
+++ main.py
@@ -7,28 +7,52 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    reps += 1
+
+    # 2nd, 4th and 6th rep
+    if reps % 2 == 0:
+        text1.config(text="Rest", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=PINK)
+        count_down(short_break_sec)
+
+    elif reps % 8 == 8:
+        # 8th rep
+        text1.config(text="Break", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=RED)
+        count_down(long_break_sec)
+    else:
+        # 1st, 3rd, 5th and 7th rep
+        text1.config(text="Work", font=(FONT_NAME, 40, "bold"), bg=YELLOW, fg=GREEN)
+        count_down(work_sec)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    canvas.itemconfig(timer_text, text=count)
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
+    # ---------------------------- UI SETUP ------------------------------- #
 
 
-# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomelo")
 window.config(padx=100, pady=50, bg=YELLOW)
